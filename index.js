314a0e8b73b23cf5aba7daadc10a42cff6308e49
trandung2k1 <trandungitksnb2001@gmail.com>
2024-06-05 20:02:49 +0700

    add about route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/about', (req, res) => {
+    res.send('About route!');
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`);
